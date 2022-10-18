9f82651eec2194f9b953998365265f158ff309e7
Inumoh Temiloluwa <temiloluwainumoh@gmail.com>
2022-10-18 12:31:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {  
+
+    let randomEmoji1 = Math.floor(Math.random() * fighters.length)
+    let randomEmoji2 = Math.floor(Math.random() * fighters.length)
+    stageEl.textContent = fighters[randomEmoji1] + " VS " + fighters[randomEmoji2]
+})
+
